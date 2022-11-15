65f0dd67cfd45eaefb6e32757546ae465cbaac07
nadia <nstoffel@sitezeus.com>
2022-11-14 23:07:59 -0500

    works but now the branch is cropped cuz of overflow auto . ugh

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex h-screen overflow-hidden relative">
+    <div class="flex h-screen relative">
         <div class="floral w-10 bg-red-100 flex-none"></div>
         <Transition name="main" @enter="mainAfterEnter" @leave="mainAfterLeave">
             <main class="flex flex-col justify-end bg-default-dark" v-show="data.mainOpen">
@@ -11,18 +11,23 @@
             </main>
         </Transition>
 
-        <div class="the-page-outer bg-dark-1 flex-1 relative flex flex-col justify-center">
+        <div class="the-page-outer bg-dark-1 flex-1 relative flex flex-col justify-center overflow-auto">
             <!-- <Transition name="branch">
                 <div class="branch" v-show="data.branchOpen"></div>
             </Transition> -->
+            <div class="the-branch" :style="`transform:translateY(${data.branchPos}%)`">
+                <div class="branch"></div>
+                <NuxtPage class="the-page text-default-light" />
+            </div>
             
-            <p class="text-default-light">
+            
+            <p class="text-default-light fixed">
                 <a class="text-inherit" @click.prevent="data.mainOpen = !data.mainOpen" href="#">Toggle</a>
             </p>
-            <div class="the-page p-10">
-                <div class="branch" v-show="data.branchOpen"></div>
+            <!-- <div class="the-page p-10">
+                
                 <NuxtPage ref="thepage" class="text-default-light bg-yellow-200" />
-            </div>
+            </div> -->
             
         </div>
     </div>
@@ -30,9 +35,12 @@
 
 <script setup>
 
+const route = useRoute()
+
 const data = reactive({
     mainOpen: false,
-    branchOpen: false
+    branchOpen: false,
+    branchPos: 50,
 })
 
 /* <div class="container mx-auto">
@@ -53,8 +61,27 @@ const data = reactive({
     onMounted(() => {
         //console.log('mounted')
         data.mainOpen = true
+
+        //console.log('route', route)
         
     })
+
+    // watch(
+    //   () => route.name.value,
+    //    newId => {
+    //     console.log(newId)
+    //   }
+    // )
+
+   watch(() => route.name, (theroute) => {
+        if (theroute === 'index') {
+            data.branchPos = 70
+        } else if (theroute === 'about') {
+            data.branchPos = 30
+        } else if (theroute === 'projects') {
+            data.branchPos = 50
+        }
+   })
 
  
 
@@ -62,22 +89,29 @@ const data = reactive({
 
 <style lang="scss">
 
+.the-branch {
+    position: absolute;
+    left: 50px;
+    top: 0;
+    bottom: 0;
+    transition: transform .5s ease-in;
+    width: 500px;
+}
+
 // .the-page {
-//     > * {
-//         position: absolute;
-//     }
+//     position: absolute;
 // }
 
 .page-enter-active,
 .page-leave-active {
-    // opacity: 1;
+    opacity: 1;
     transition: all .5s;
     transform: translateY(0)
 }
 
 .page-enter-from,
 .page-leave-to {
-    // opacity: 0;
+    opacity: 0;
     transform: translateY(10px)
 }
 
@@ -86,7 +120,7 @@ main {
 
     &.main-enter-active,
     &.main-leave-active {
-        transition: width 2s;
+        transition: width .5s;
         @apply w-80; 
     
     }
@@ -99,6 +133,8 @@ main {
 
 .branch {
     // position: absolute;
+    @apply mb-10;
+
     position: relative;
     // top: 300px;
      left: -80px;
