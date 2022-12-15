2eac1f1f7eae0173ffd7be7a113c175222ca73bb
nadia <nstoffel@sitezeus.com>
2022-12-14 22:21:42 -0500

    try re-using masterTL.. with .clear()

--- components/layout/layoutScreen.vue
+++ components/layout/layoutScreen.vue
@@ -1,41 +1,41 @@
 <template>
-        <div class="flex h-screen relative">
-            <div class="first floral flex-none" ref="first"></div>
-            <div class="second bg-default-dark" ref="second"></div>
-            <div class="third the-page-outer bg-dark-1 flex-1 relative flex flex-col justify-center overflow-auto">
-                <div class="the-page p-10" ref="thepage">
-                    <NuxtPage class="text-default-light"  />
-                </div>
+    <div class="flex h-screen relative">
+        <div class="first floral flex-none" ref="first"></div>
+        <div class="second bg-default-dark" ref="second"></div>
+        <div class="third the-page-outer bg-dark-1 flex-1 relative flex flex-col justify-center overflow-auto">
+            <div class="the-page p-10" ref="thepage">
+                <NuxtPage class="text-default-light"  />
             </div>
-            <div class="standard fixed flex flex-col justify-between h-full">
-                <div class="style-logo flex-col text-center" ref="logo">
-                    <StyleLogo 
-                        class="logo" 
-                        @timelines="childTimelines"
-                    />
-                    <p class="blurb" ref="blurb">A portfolio site by <nuxt-link>Nadia Chu</nuxt-link></p>
-                </div>
-                
-                <nav class="flex justify-center gap-4 m-10" ref="standardnav">
-                    <a href="#" @click="goTo('index')">Index</a>
-                    <a href="#" @click="goTo('about')">About</a>
-                    <a href="#" @click="goTo('projects')">Projects</a>
-                </nav>
-            </div>
-            <div class="full" ref="fullnav">
-                <nav>
-                    <span>logo</span>
-                    <span>Nadia Chu</span>
-                </nav>
-                <nav class="nav flex justify-center gap-4">
-                    <a href="#" @click="goTo('index')">Index</a>
-                    <a href="#" @click="goTo('about')">About</a>
-                    <a href="#" @click="goTo('projects')">Projects</a>
-                </nav>
-            </div>
-            <button @click="blinkingTL.play()">play</button>
-            <button @click="blinkingTL.pause()">pause</button>
         </div>
+        <div class="standard fixed flex flex-col justify-between h-full">
+            <div class="style-logo flex-col text-center" ref="logo">
+                <StyleLogo 
+                    class="logo" 
+                    @timelines="childTimelines"
+                />
+                <p class="blurb" ref="blurb">A portfolio site by <nuxt-link>Nadia Chu</nuxt-link></p>
+            </div>
+            
+            <nav class="flex justify-center gap-4 m-10" ref="standardnav">
+                <a href="#" @click="goTo('index')">Index</a>
+                <a href="#" @click="goTo('about')">About</a>
+                <a href="#" @click="goTo('projects')">Projects</a>
+            </nav>
+        </div>
+        <div class="full" ref="fullnav">
+            <nav>
+                <span>logo</span>
+                <span>Nadia Chu</span>
+            </nav>
+            <nav class="nav flex justify-center gap-4">
+                <a href="#" @click="goTo('index')">Index</a>
+                <a href="#" @click="goTo('about')">About</a>
+                <a href="#" @click="goTo('projects')">Projects</a>
+            </nav>
+        </div>
+        <!-- <button @click="blinkingTL.play()">play</button>
+        <button @click="blinkingTL.pause()">pause</button> -->
+    </div>
 </template>
 
 
@@ -60,34 +60,29 @@ const data = reactive({
 
 })
 
-
+let masterTL
 let fullTL, logoTL, blinkingTL, revealPageTL
 
 const goTo = (to) => {
-
     const from = route.name
-    //console.log('goTo: route:', route.name)
 
     if (from !== to) {
-        //console.log('goTo: from != to')
-
-        //route changed, so remove the current page
-        const routeTL = gsap.timeline({paused: true})
+        masterTL
+            .clear()
             .add(revealPageTL.reverse())
 
         if (to === 'projects') {
-            routeTL.add(reverse().play())
-            routeTL.add(fullTL.play())
+            masterTL.add(standardBackTL().play())
+            masterTL.add(fullTL.play())
         }
 
         if (from === 'projects' ) {
-            routeTL.add(fullTL.reverse())
-            routeTL.add(forward().play())
+            masterTL.add(fullTL.reverse())
+            masterTL.add(standardForwardTL().play())
         }
 
         const path = to === 'index' ? '/' : '/' + to
-        routeTL.add(() => router.push({ path: path }))
-        routeTL.play()
+        masterTL.add(() => router.push({ path: path }))
     }
 
 }
@@ -99,6 +94,8 @@ const childTimelines = (payload) => {
 }
 
 const setupTLs = () => {
+
+    masterTL = gsap.timeline()
 
     fullTL = gsap.timeline({
         paused: true
@@ -113,17 +110,16 @@ const setupTLs = () => {
         .to(thepage.value, { opacity: 1 })
 }
 
-const forward = () => {
+const standardForwardTL = () => {
     return gsap.timeline({ paused: true })
         .to(first.value, { width: '50px' })
         .to(second.value, { width: '300px' })
         .add(logoTL.timeScale(1).restart().play(), '>-50%')
         .to(blurb.value, { opacity: 1 })
         .to(standardnav.value.children, {opacity: 1, stagger:.2})
-        
 }
 
-const reverse = () => {
+const standardBackTL = () => {
     return gsap.timeline({ paused: true })
         .to(standardnav.value.children, {opacity: 0, stagger:.2})
         .to(blurb.value, { opacity: 0 })
@@ -138,25 +134,19 @@ onMounted(() => {
     setupTLs()
 
     if (['projects'].includes(route.name)) {
-        data.layoutType = 'full'
-        gsap.timeline()
+        masterTL
             .add(fullTL.play())
             .add(revealPageTL.play())
     } else {
-        data.layoutType = 'standard'
-        gsap.timeline()
-            .add(forward().play())
+        masterTL
+            .add(standardForwardTL().play())
             .add(revealPageTL.play())
     }
 })
 
-//possibly move this to middleware
-
 watch(() => route.name, (to, from) => {
-    //console.log('watch: route:', route.name)
     gsap.timeline()
         .add(revealPageTL.play())
-
 })
 
 
